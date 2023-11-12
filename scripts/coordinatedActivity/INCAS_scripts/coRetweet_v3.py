7ffe0be1822d81da91f90517e07f17affc8b2c98
sinking8 <ashwinblaze111@gmail.com>
2023-11-12 15:37:29 -0800

    updated coretweet

--- scripts/coordinatedActivity/INCAS_scripts/coRetweet_v3.py
+++ scripts/coordinatedActivity/INCAS_scripts/coRetweet_v3.py
@@ -16,7 +16,7 @@ import warnings
 # Data assumptions:
 #   - 2 Pandas dataframes
 #     - control: control dataset -> includes only columns ['user', 'retweeted_status', 'id']
-#     - treated: information Operation dataset -> includes only columns ['tweetid', 'userid', 'retweet_tweetid']
+#     - treated: information Operation dataset -> includes only columns ['tweetid', 'userid', 'retweet_id']
 
 
 # Data Assumption - INCAS
@@ -40,55 +40,43 @@ def coRetweet(cum):
     
     cum = cum.rename(index=str,columns={'id':'tweetid'})
 
-    # Sorting cum based on timePublished
-    cum.sort_values(by=['timePublished'], inplace=True)
-    
-    warnings.warn("No of Records "+str(len(cum)))
-    
-    filt = cum[['userid', 'tweetid']].groupby(
-        ['userid'], as_index=False).count()
-
-    filt = list(filt.loc[filt['tweetid'] >= 10]['userid'])
-    # print(filt)
+    filt = cum[['userid', 'tweetid']].groupby(['userid'],as_index=False).count()
+    filt = list(filt.loc[filt['tweetid'] >= 20]['userid'])
     cum = cum.loc[cum['userid'].isin(filt)]
     cum = cum[['userid', 'retweet_id']].drop_duplicates()
 
     temp = cum.groupby('retweet_id', as_index=False).count()
-    cum = cum.loc[cum['retweet_id'].isin(
-        temp.loc[temp['userid'] > 1]['retweet_id'].to_list())]
+    cum = cum.loc[cum['retweet_id'].isin(temp.loc[temp['userid']>1]['retweet_id'].to_list())]
 
     cum['value'] = 1
+    
+    ids = dict(zip(list(cum.retweet_id.unique()), list(range(cum.retweet_id.unique().shape[0]))))
+    cum['retweet_id'] = cum['retweet_id'].apply(lambda x: ids[x]).astype(int)
+    del ids
 
-    ids = dict(zip(list(cum.retweet_id.unique()), list(
-        range(cum.retweet_id.unique().shape[0]))))
-    cum['retweet_id'] = cum['retweet_id'].apply(
-        lambda x: ids[x]).astype(int)
-    # del urls
-    warnings.warn("CUM "+ str(len(set(cum['userid']))))
-
+    userid = dict(zip(list(cum.userid.astype(str).unique()), list(range(cum.userid.unique().shape[0]))))
+    cum['userid'] = cum['userid'].astype(str).apply(lambda x: userid[x]).astype(int)
+    
     person_c = CategoricalDtype(sorted(cum.userid.unique()), ordered=True)
     thing_c = CategoricalDtype(sorted(cum.retweet_id.unique()), ordered=True)
-
+    
     row = cum.userid.astype(person_c).cat.codes
     col = cum.retweet_id.astype(thing_c).cat.codes
-    sparse_matrix = csr_matrix((cum["value"], (row, col)), shape=(
-        person_c.categories.size, thing_c.categories.size))
+    sparse_matrix = csr_matrix((cum["value"], (row, col)), shape=(person_c.categories.size, thing_c.categories.size))
     del row, col, person_c, thing_c
-
+    
     vectorizer = TfidfTransformer()
     tfidf_matrix = vectorizer.fit_transform(sparse_matrix)
     similarities = cosine_similarity(tfidf_matrix, dense_output=False)
 
+
     df_adj = pd.DataFrame(similarities.toarray())
     del similarities
-    #uniques = set(list(le.inverse_transform(cum['userid'].values)))
-    df_adj.index = ids.keys()
-    df_adj.columns = ids.keys()
-    #df_adj.index = uniques
-    #df_adj.columns = uniques
+    df_adj.index = userid.keys()
+    df_adj.columns = userid.keys()
     G = nx.from_pandas_adjacency(df_adj)
     del df_adj
-
+    
     G.remove_nodes_from(list(nx.isolates(G)))
 
     return G
