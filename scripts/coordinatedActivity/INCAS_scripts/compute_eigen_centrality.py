0c20c37f9e27d3f638a085714ed5e30ef495538a
sinking8 <ashwinblaze111@gmail.com>
2023-11-16 22:00:57 -0800

    updated scripts for eigen

--- scripts/coordinatedActivity/INCAS_scripts/compute_eigen_centrality.py
+++ scripts/coordinatedActivity/INCAS_scripts/compute_eigen_centrality.py
@@ -1,14 +1,35 @@
 import networkx as nx
+from scipy.stats import norm
+import matplotlib.pyplot as plt
 import os
-gephi_DIR = "../../../gephi"
-graph_DIR = "coRetweet_INCAS.gexf"
 
-G = nx.read_gexf(os.path.join(gephi_DIR,graph_DIR))
+INCAS_DIR = "/scratch1/ashwinba/cache/INCAS"
 
-centrality = nx.eigenvector_centrality(G)
-centrality_dict =  {key:{'eigen_centrality':centrality[key]} for key in list(centrality.keys())}
+def compute_eigen(graph_dir,method):
+    G  = nx.read_gexf(os.path.join(graph_dir))
+    centrality = nx.eigenvector_centrality(G)
+    plot_distribution_curve(list(centrality.values()),method)
+    centrality_dict =  {key:{'eigen_centrality':centrality[key]} for key in list(centrality.keys())}
+    nx.set_node_attributes(G,centrality_dict)
+    nx.write_gexf(G,os.path.join(INCAS_DIR,"{METHOD}_INCAS_0908_eigen.gexf".format(METHOD=method)))
 
-# Eigen Parameter
+def plot_distribution_curve(values,method):
+    values.sort()
+    print(values[-1])
+    norms  = norm.pdf(values)
+    print(norms[-1])
+    plt.plot(values,norms)
+    plt.ylabel("density")
+    plt.xlabel("eigen-centrality")
+    plt.savefig(os.path.join(INCAS_DIR,"{METHOD}.png".format(METHOD=method)))
+    plt.show()
 
-nx.set_node_attributes(G,centrality_dict)
-nx.write_gexf(G,os.path.join(gephi_DIR,"updated.gexf"))
+graph_dir = "../../../gephi/coRetweet_INCAS.gexf"
+compute_eigen(graph_dir,"coRetweet")
+
+
+
+
+
+
+
