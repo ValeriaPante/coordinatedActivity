de45fb6589ca1f04fbbbc311b32b816f9881ec9b
sinking8 <ashwinblaze111@gmail.com>
2023-11-22 16:20:35 -0800

    updated

--- scripts/coordinatedActivity/INCAS_scripts/fastRetweet_v3.py
+++ scripts/coordinatedActivity/INCAS_scripts/fastRetweet_v3.py
@@ -57,6 +57,7 @@ def fastRetweet(cum1, timeInterval = 10):
     cumulative = cumulative.loc[cumulative['delta'] > 1]
     
     cum = nx.from_pandas_edgelist(cumulative, 'userid', 'retweet_userid','delta')
+    print(cum.columns)
 
     cum['userid'].astype(int).astype(str)
     cum = cum.loc[cum['delta'] > 1]
