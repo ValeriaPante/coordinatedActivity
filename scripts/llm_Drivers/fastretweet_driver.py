727fe0a6238de5732294ff345cf62cab726ce601
sinking8 <ashwinblaze111@gmail.com>
2023-12-11 16:20:53 -0800

    added fastretweet driver

---
+++ scripts/llm_Drivers/fastretweet_driver.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+import pickle
+import networkx as nx
+
+import gzip
+import warnings
+
+# Importing coordinatedActivity root directory
+sys.path.append('/scratch1/ashwinba/coordinatedActivity/scripts')
+
+from coordinatedActivity.INCAS_scripts.fastRetweet_v3 import *
+
+# Declare directories and file_name
+dataset_dir = "/scratch1/ashwinba/data" # File Location
+graph_dir = "/scratch1/ashwinba/cache/llms" #Final destination of graph
+file_name = "df_train_russia.csv" # Name of the File to be read
+country_name = file_name.split("_")[-1].split(".")[0]
+
+
+with gzip.open(os.path.join(dataset_dir,file_name)) as f:
+    cum_df = pd.read_csv(f)
+
+warnings.warn("opened dataframe")
+G = fastRetweet(cum_df)
+
+# Saving Graph in GML File
+nx.write_gexf(G,os.path.join(graph_dir,"fastretweet_llms{COUNTRY}.gexf".format(COUNTRY=country_name)))
+warnings.warn("file written")
+
+
